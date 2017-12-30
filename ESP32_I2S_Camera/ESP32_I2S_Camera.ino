62f67a54ba32d7f8c0e40cd6b2763e366f7d089b
ERIK2807 <kirefabi@gmail.com>
2017-12-30 16:38:19 -0500

    Update ESP32_I2S_Camera.ino

--- ESP32_I2S_Camera/ESP32_I2S_Camera.ino
+++ ESP32_I2S_Camera/ESP32_I2S_Camera.ino
@@ -1,7 +1,7 @@
 #include "OV7670.h"
 
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library
+//#include <Adafruit_GFX.h>    // Core graphics library
+//#include <Adafruit_ST7735.h> // Hardware-specific library
 
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -11,36 +11,58 @@
 const int SIOD = 21; //SDA
 const int SIOC = 22; //SCL
 
-const int VSYNC = 34;
-const int HREF = 35;
+const int VSYNC = 39;
+const int HREF = 36;
 
 const int XCLK = 32;
 const int PCLK = 33;
 
-const int D0 = 27;
-const int D1 = 17;
-const int D2 = 16;
-const int D3 = 15;
-const int D4 = 14;
-const int D5 = 13;
-const int D6 = 12;
-const int D7 = 4;
+const int D0 = 13;
+const int D1 = 12;
+const int D2 = 14;
+const int D3 = 27;
+const int D4 = 26;
+const int D5 = 25;
+const int D6 = 35;
+const int D7 = 34;
 
-const int TFT_DC = 2;
-const int TFT_CS = 5;
-//DIN <- MOSI 23
-//CLK <- SCK 18
 
-#define ssid1        "YOUR_WIFI_SSID"
-#define password1    "YOUR_PASSWORD"
+#define ssid1        "ssid1"
+#define password1    "password1"
 //#define ssid2        ""
 //#define password2    ""
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, 0/*no reset*/);
+IPAddress myIP       = IPAddress(192,168,0,98);  // IP para conectarse a la red 
+IPAddress myGateway = IPAddress(192,168,0, 1);
+
+
+
 OV7670 *camera;
 
 WiFiMulti wifiMulti;
 WiFiServer server(80);
+
+bool wifi_connect(){
+  WiFi.config( myIP, myGateway, IPAddress(255,255,255,0));  
+  wifiMulti.addAP(ssid1,password1);
+  //wifiMulti.addAP(ssid2,password2);
+  
+
+
+  //Serial.println(F("Connecting Wifi..."));
+  if(wifiMulti.run() == WL_CONNECTED) {
+
+          
+      /*//Serial.println(F("--- WiFi connected ---"));
+      //Serial.print(F("SSID: "));
+      //Serial.println( WiFi.SSID() );
+      //Serial.print(F("IP Address: "));
+      //Serial.println( WiFi.localIP() );*/
+      
+    return true;
+  }
+  else return false;
+}
 
 unsigned char bmpHeader[BMP::headerSize];
 
@@ -65,7 +87,7 @@ void serve()
             client.println("Content-type:text/html");
             client.println();
             client.print(
-              "<style>body{margin: 0}\nimg{height: 100%; width: auto}</style>"
+              "<style>body{margin: 0}\nimg{height: 75%; width: auto}</style>"
               "<img id='a' src='/camera' onload='this.style.display=\"initial\"; var b = document.getElementById(\"b\"); b.style.display=\"none\"; b.src=\"camera?\"+Date.now(); '>"
               "<img id='b' style='display: none' src='/camera' onload='this.style.display=\"initial\"; var a = document.getElementById(\"a\"); a.style.display=\"none\"; a.src=\"camera?\"+Date.now(); '>");
             client.println();
@@ -87,10 +109,8 @@ void serve()
             client.println("Content-type:image/bmp");
             client.println();
             
-            for(int i = 0; i < BMP::headerSize; i++)
-               client.write(bmpHeader[i]);
-            for(int i = 0; i < camera->xres * camera->yres * 2; i++)
-               client.write(camera->frame[i]);
+            client.write(bmpHeader, BMP::headerSize);
+            client.write(camera->frame, camera->xres * camera->yres * 2);
         }
       }
     }
@@ -102,29 +122,19 @@ void serve()
 
 void setup() 
 {
-  Serial.begin(115200);
-
-  wifiMulti.addAP(ssid1, password1);
-  //wifiMulti.addAP(ssid2, password2);
-  Serial.println("Connecting Wifi...");
-  if(wifiMulti.run() == WL_CONNECTED) {
-      Serial.println("");
-      Serial.println("WiFi connected");
-      Serial.println("IP address: ");
-      Serial.println(WiFi.localIP());
+       
+  if(wifi_connect()){ 
+      server.begin();     
   }
   
   camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
   BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
   
-  tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(0);
-  server.begin();
 }
 
 void displayY8(unsigned char * frame, int xres, int yres)
 {
-  tft.setAddrWindow(0, 0, yres - 1, xres - 1);
+
   int i = 0;
   for(int x = 0; x < xres; x++)
     for(int y = 0; y < yres; y++)
@@ -134,19 +144,17 @@ void displayY8(unsigned char * frame, int xres, int yres)
       unsigned short r = c >> 3;
       unsigned short g = c >> 2;
       unsigned short b = c >> 3;
-      tft.pushColor(r << 11 | g << 5 | b);
+
     }  
 }
 
 void displayRGB565(unsigned char * frame, int xres, int yres)
 {
-  tft.setAddrWindow(0, 0, yres - 1, xres - 1);
   int i = 0;
   for(int x = 0; x < xres; x++)
     for(int y = 0; y < yres; y++)
     {
       i = (y * xres + x) << 1;
-      tft.pushColor((frame[i] | (frame[i+1] << 8)));
     }  
 }
 
